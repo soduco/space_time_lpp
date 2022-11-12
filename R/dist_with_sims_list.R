3d5ca4deac6dc2c3eed5ed3485b675028586adb4
Julie Gravier <JGravier@users.noreply.github.com>
2022-11-12 12:41:07 +0100

    update dist sim lists
    renaming > one .R file for on function

--- R/dist_with_sims_list.R
+++ R/dist_with_sims_list.R
@@ -1,62 +1,59 @@
-#' Shortest paths distances between points (in class lists) on planar networks (in class lists)
+#' Shortest paths distances between points (class list) on planar networks (class list)
 #'
 #' @description
-#' Computation of shortest path distances between all observed points of a planar networks and
+#' Computation of shortest path distances between all observed points of planar networks and
 #' shortest paths distances between randomly simulated points on the same planar networks.
-#' Input data point patterns must be snapped on linear networks.
+#' Input point patterns data must be snapped on linear networks.
 #' The length and order of lists of points and planar networks must be equivalent and consistent.
 #'
 #' @param listpp A list of sf point patterns
-#' @param listnetwork A list of sf planar networks lines
-#' @param nsim The number of simulations chosen for randomly simulated points (must be > 1)
+#' @param listnetwork A list of sf planar networks
+#' @param nsim The number of simulations generating randomly simulated points (must be > 1)
 #' @return A list of tibble of shortest paths
 #'
-#' @details Pi/P points numbers in output tibbles refer to initial order of rows in point patterns data.
+#' @details 
+#' * `Pi`/`P` points numbers in output tibbles refer to initial order of rows in point patterns data.
+#' * `sim` column refers to the to the number of a specific simulation and the 
+#' `type` to the calculated distance depending on whether the points are simulated or observed.
 #'
-#' @seealso Snapping points on lines: \code{\link[maptools::snapPointsToLines]{maptools::snapPointsToLines()}}
+#' @seealso [snapPointsToLines()] for snapping points on lines.
 #'
 #' @examples
-#' blob
+#' blobi
 #'
 #' @importFrom Rdpack reprompt
 #' @export
 
 dist_with_sims_list <- function(listpp, listnetwork, nsim) {
 
-  # expressive data
-  list_patterns_snap <- listpp
-  list_of_networks <- listnetwork
-
-  #### create random points on network and  calculating distances ####
-  nsimulation <- nsim # expressive
-
+  #### create random points on network and calculating distances ####
   ## generate points on a linear network
   liste_rss_csr <- list()
 
-  for (i in 1:length(list_patterns_snap)) {
+  for (i in 1:length(listpp)) {
     list_calcul <- spatstat.linnet::runiflpp(
-      n = nrow(list_patterns_snap[[i]]), # generate same N points as observed data
-      L = maptools::as.linnet.SpatialLines(X = as(list_of_networks[[i]], "Spatial")),
-      nsim = nsimulation
+      n = nrow(listpp[[i]]), # generate same N points as observed data
+      L = maptools::as.linnet.SpatialLines(X = as(listnetwork[[i]], "Spatial")),
+      nsim = nsim
     )
 
     liste_rss_csr[[i]] <- list_calcul
   }
 
-  ### calculating shortest paths for simulations
-  list_dist_pi_p <- list()
+  ## calculating shortest paths for simulations
+  list_simulated_dist <- list()
 
   for (i in 1:length(liste_rss_csr)) {
     liste_rss_csr_extract <- lapply(liste_rss_csr[[i]], `[`, c())
 
-    ma_liste_simulation <- list()
+    list_dist_pi_p <- list()
 
     for (j in 1:length(liste_rss_csr_extract)) {
       dist_pi_p <- spatstat.geom::pairdist(X = liste_rss_csr_extract[[j]])
 
       dist_pi_p[upper.tri(x = dist_pi_p, diag = TRUE)] <- NA
 
-      ma_liste_simulation[[j]] <- dist_pi_p %>%
+      list_dist_pi_p[[j]] <- dist_pi_p %>%
         tibble::as_tibble() %>%
         tibble::rowid_to_column(var = "Pi") %>%
         tidyr::pivot_longer(cols = -Pi, names_to = "P", values_to = "dist_pi_p") %>%
@@ -65,17 +62,17 @@ dist_with_sims_list <- function(listpp, listnetwork, nsim) {
         dplyr::mutate(sim = j)
     }
 
-    list_dist_pi_p[[i]] <- ma_liste_simulation
+    list_simulated_dist[[i]] <- list_dist_pi_p
   }
 
   #### Calculating distances on observed point patterns ####
   list_observed_dist <- list()
 
-  for (i in 1:length(list_patterns_snap)) {
+  for (i in 1:length(listpp)) {
     # creation of an lpp object
     observed_lpp <- spatstat.linnet::lpp(
-      X = spatstat.geom::as.ppp(X = list_patterns_snap[[i]] %>% dplyr::select(geometry)),
-      L = maptools::as.linnet.SpatialLines(X = as(list_of_networks[[i]], "Spatial"))
+      X = spatstat.geom::as.ppp(X = listpp[[i]] %>% dplyr::select(geometry)),
+      L = maptools::as.linnet.SpatialLines(X = as(listnetwork[[i]], "Spatial"))
     )
 
     matrix_observed_dist_pi_p <- spatstat.geom::pairdist(X = observed_lpp)
@@ -92,16 +89,20 @@ dist_with_sims_list <- function(listpp, listnetwork, nsim) {
   }
 
   ### list of output tibbles ####
-  # simulation and observed shortest paths
+  # simulated and observed shortest paths
   list_result_shortest_path_matrices <- list()
 
-  for (i in 1:length(list_dist_pi_p)) {
-    tableau_init <- data.table::rbindlist(l = list_dist_pi_p[[i]]) %>%
-      tibble::as_tibble(.name_repair = "unique") %>%
-      dplyr::bind_rows(list_observed_dist[[i]])
+  for (i in 1:length(list_simulated_dist)) {
+    final_tibble <- data.table::rbindlist(l = list_simulated_dist[[i]]) %>%
+      tibble::as_tibble(.name_repair = "minimal") %>%
+      dplyr::mutate(type = "simulation") %>%
+      dplyr::bind_rows(
+        list_observed_dist[[i]] %>%
+          dplyr::mutate(type = "observation")
+      )
 
 
-    list_result_shortest_path_matrices[[i]] <- tableau_init
+    list_result_shortest_path_matrices[[i]] <- final_tibble
   }
 
   return(list_result_shortest_path_matrices)
